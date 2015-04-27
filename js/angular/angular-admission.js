3e9339721c5921db475e08654a2fa215dcac881a
Sule-odu Adedayo <suleodu.adedayo@gmail.com>
2015-04-27 20:20:12 +0100

    Controllers/admission and payment  - as at 4/16/2015 3:42

--- js/angular/angular-admission.js
+++ js/angular/angular-admission.js
@@ -5,6 +5,8 @@
  */
 var admissionModule = angular.module('tams-app', []);
 
+
+
 admissionModule.controller('PageController', function($scope){
     $scope.lga = lga;
     $scope.exam_groups = exam_groups;
@@ -57,47 +59,38 @@ admissionModule.controller('PageController', function($scope){
     };//END of State and local Govt selection.
     
     
+    $scope.gen4repeat = function(val) {
+        
+        var dt = new Array(); 
+        for(i = 0; i < val; i++){
+            dt[i] = 'item_'+i;
+        }
+        return dt;
+    };// End of repeat element
     
-    $scope.group_exam = null
-    $scope.$watch('exam_group', function() {
-        $scope.get_exam_data($scope.exam_group);  
+  
+    $scope.group_exam = null 
+    $scope.$watch('group', function() {
+        
+        console.log("here");
+        
+        
     });
     
     
-    $scope.get_exam_data = function(value){
-        var ex_group;
-        var ex_typ_period;
+   $scope.get_exam_data =  function (value){ 
+        var ex_type = _.filter($scope.exam_type_period, {'groupid' : value});
         
-        
-        ex_group = _.filter($scope.exam_groups, {'groupid' : value});
-        ex_typ_period = _.filter($scope.exam_type_period, {'groupid' : value});
-        
-        $scope.group_exam = ex_group;
-        $scope.ex_typ_periods = ex_typ_period;
-        
-        //console.log($scope.ex_typ_periods);
-        //console.log($scope.group_exam);
+        return ex_type;
     };
     
     
-    $scope.subject1 = null;
-    $scope.grade1 = null;
     
-    $scope.$watch('exam_type1', function() {
-        $scope.get_exam_subject1($scope.exam_type1);  
-    });
     
-    $scope.get_exam_subject1 = function(value){
-       var ex_subject1;
-       var ex_grade1;
-       
-       ex_subject1 = _.filter($scope.exam_subject, {'examid' : value});
-       ex_grade1 = _.filter($scope.exam_grades, {'examid' : value});
-       $scope.subject1 = ex_subject1;
-       $scope.grade1 = ex_grade1;
-       
-       console.log($scope.subject1);
-    };
+    
+    
+    
+    
     
     $scope.subject2 = null;
     $scope.grade2 = null;
@@ -106,17 +99,18 @@ admissionModule.controller('PageController', function($scope){
         $scope.get_exam_subject2($scope.exam_type2);  
     });
     
-    $scope.get_exam_subject2 = function(value){
-       var ex_subject2;
-       var ex_grade2;
-       
-       ex_subject2 = _.filter($scope.exam_subject, {'examid' : value});
-       ex_grade2 = _.filter($scope.exam_grades, {'examid' : value});
-       $scope.subject2 = ex_subject2;
-       $scope.grade2 = ex_grade2;
-       
-       console.log($scope.subject2);
-    };
+//    $scope.get_exam_subject2 = function(value){
+//       var ex_subject2;
+//       var ex_grade2;
+//       
+//       ex_subject2 = _.filter($scope.exam_subject, {'examid' : value});
+//       ex_grade2 = _.filter($scope.exam_grades, {'examid' : value});
+//       $scope.subject2 = ex_subject2;
+//       $scope.grade2 = ex_grade2;
+//       
+//       console.log($scope.subject2);
+//    };
+    
     
     
 });
